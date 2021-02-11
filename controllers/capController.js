3a4c6dbfc9f671d71a5f3ae8940d7027e2dd3d67
Abhijeet Bhagat <bhagat.abhijeet@gmail.com>
2021-02-11 14:21:48 -0800

    change the body payload to {config:{},payload:{},post:boolean} form for cap

--- controllers/capController.js
+++ controllers/capController.js
@@ -1,5 +1,5 @@
 const capgen = require("capgen");
-const util = require('util');
+const util = require("util");
 const { builder, create, fragment } = require("xmlbuilder2");
 // var parser = require('xml2js');
 const { JSDOM } = require("jsdom");
@@ -8,52 +8,60 @@ const fs = require("fs");
 const writeFileAsync = util.promisify(fs.writeFile);
 // const fsReadFileAsync= util.promisify(fs.readFile);
 
-
-function getTextFromFile(filePath){
+function getTextFromFile(filePath) {
   let fileData;
-  try{
-   fileData = fs.readFileSync(filePath);
-  //  console.log(fileData.toString());
-   return fileData.toString();
-  }
-  catch(err){
+  try {
+    fileData = fs.readFileSync(filePath);
+    //  console.log(fileData.toString());
+    return fileData.toString();
+  } catch (err) {
     throw err;
   }
-  
 }
 
 async function jsonToCap(req, res) {
-  const config = {
-    strictMode: false,
-    comment: false,
-    xmlOptions: {
-      headless: true,
-      prettyPrint: true,
-    },
-  };
+  const body = req.body;
+  let config, shouldBePosted;
+  if (body.hasOwnProperty("config") && body.config !== null) {
+    config = body.config;
+  } else {
+    config = {
+      strictMode: false,
+      comment: false,
+      blah: 1,
+      xmlOptions: {
+        headless: true,
+        prettyPrint: true,
+      },
+    };
+  }
+
   const gen = new capgen.Capgen(config);
-  // const jsonObj=req.body
-  // console.log(jsonObj);
-  const xml = gen.createUsing(req.body);
 
-  const capfeedfilepath = path.join(__dirname, "../public", "capfeed.cap");
-  console.log(capfeedfilepath);
+  if (body.hasOwnProperty("post") && body.post !== null) {
+    shouldBePosted = body.post;
+  } else {
+    shouldBePosted = false;
+  }
+  const xml = gen.createUsing(req.body.payload);
 
-  let capxmlFromFile = await getTextFromFile(capfeedfilepath);
+  if (shouldBePosted) {
+    if (typeof xml === "object") {
+      console.log("Invalid xml!");
+    } else {
+      const capfeedfilepath = path.join(__dirname, "../public", "capfeed.cap");
+      console.log(capfeedfilepath);
 
-    
- 
-  // **** working ****
-  // const dom = new JSDOM("");
-  // const DOMParser = dom.window.DOMParser;
-  // const parser = new DOMParser;
-  // let doc = parser.parseFromString(capxmlFromFile, "application/xml")
-  const alerts = create(capxmlFromFile);
-  const alert = fragment(xml);
-  alerts.node.firstChild.appendChild(alert.node);
-   
-  await writeFileAsync(capfeedfilepath,alerts.toString(),'UTF8');
-  console.log(alerts.toString());
+      let capxmlFromFile = await getTextFromFile(capfeedfilepath);
+
+      const alerts = create(capxmlFromFile);
+      const alert = fragment(xml);
+      alerts.node.firstChild.appendChild(alert.node);
+
+      await writeFileAsync(capfeedfilepath, alerts.toString(), "UTF8");
+      console.log(alerts.toString());
+    }
+  }
   res.send(xml);
 }
 
